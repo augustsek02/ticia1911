99a7c36790dc3e21e5ca89c1b4a46fba3fc3fe75
augustsek02 <51264804+augustsek02@users.noreply.github.com>
2021-12-27 21:40:16 +0300

    peer to peer in java
    socket network communication

---
+++ p2p/P2P messaging app Java/src/mychatappp/MyChatAppp.java
@@ -0,0 +1,45 @@
+/*
+ 
+ * and open the template in the editor.
+ */
+package mychatappp;
+
+import mychatappp.gui.MainScreen;
+
+/**
+ *
+ * @author manthanhd
+ */
+public class MyChatAppp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+       
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainScreen().setVisible(true);
+            }
+        });
+    }
+}
